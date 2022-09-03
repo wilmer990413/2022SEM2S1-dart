a0826761822235422fbf219edd386c485947832c
Wilmer Rodolfo Castro Ladino <wilmer.castro@unillanos.edu.co>
2022-09-03 14:10:01 -0500

    Exception

--- 5.excepciones/excepciones.dart
+++ 5.excepciones/excepciones.dart
@@ -1,1 +1,25 @@
-void main() {}
+void main() {
+  try {
+    String nombre = obtenerNombre("");
+    print(nombre);
+  } on FormatException catch (e) {
+    print("Se presento una excepcion FormatException = $e");
+  } on Exception catch (e){
+    print("Se presento una excepcion Exception = $e");
+  }
+  catch (e) {
+    print("Se presenta una exception general");
+  } finally {
+    print("Este es el finally");
+  }
+}
+
+String obtenerNombre(String nombre){
+  if (nombre == 'Wilmer') {
+    throw FormatException("Este nombre no esta permitido");
+  }
+  if (nombre == ""){
+    throw Exception("No ingreso un nombre");
+  }
+  return 'Mi nombre es $nombre';
+}
