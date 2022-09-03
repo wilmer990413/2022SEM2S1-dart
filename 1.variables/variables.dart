18e2341292f31f86bf371c114ab23ef1bf95f8e2
Wilmer Rodolfo Castro Ladino <wilmer.castro@unillanos.edu.co>
2022-09-03 11:34:08 -0500

    Variables

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -1,1 +1,12 @@
-void main() {}
+void main() {
+  var nombre = "wilmer";
+  String nombreTipado = "wilmer";
+
+  final String nombreFinalTipado = "wilmer";
+  const String nombreConstTipado = "wilmer";
+
+  print(nombre);
+  print(nombreTipado);
+  print(nombreFinalTipado);
+  print(nombreConstTipado);
+}
