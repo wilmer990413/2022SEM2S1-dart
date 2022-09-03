6af9feda8c135f7fae4a90cf547fc0424fe97bfd
Wilmer Rodolfo Castro Ladino <wilmer.castro@unillanos.edu.co>
2022-09-03 13:33:59 -0500

    Funciones

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,1 +1,21 @@
-void main() {}
+void main() {
+  print(obtenerNombre("Wilmer"));
+  print(obtenerNombreCompleto("Wilmer", "Castro"));
+  print(obtenerNombreCompletoPara(apellido: "Castro"));
+  print(AnonFunction("PEPEPEPE"));
+}
+String obtenerNombre(String nombre){
+  return 'My name is $nombre';
+}
+
+String obtenerNombreCompleto(String nombre, String apellido){
+  return 'My name is $nombre $apellido';
+}
+
+String obtenerNombreCompletoPara({String? nombre, required String apellido, int edad = 25}){
+  return 'My name is $nombre $apellido and I am $edad years old';
+}
+
+var AnonFunction = (String valor){
+  return "el valor es $valor";
+};
