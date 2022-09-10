21908312ddd18c208d169ed8a6a3dad8d8d65249
Wilmer Rodolfo Castro Ladino <wilmer.castro@unillanos.edu.co>
2022-09-10 00:31:00 -0500

    Ejercicio final de Clases

--- 7.ejercicios/ejercicioDeClase.dart
+++ 7.ejercicios/ejercicioDeClase.dart
@@ -1,7 +1,54 @@
 import 'dart:ffi';
 
 void main() {
-
+  Persona persona = new Persona(nombre: "Wilmer", apellido:  "Castro",edad: 23);
+  ////////////////// ARTISTA
+  Artista artista = Artista("Pedro Gallery");
+  print("Artista: " "${artista.nombre} ${artista.apellido} Edad: ${artista.edad}");
+  print("Galeria: ${artista.nomGaleria}" );
+  artista.color = "Azul";
+  artista.figura = "Circulo";
+  print(artista.dibujarFigura());
+  /////////////////// INGENIERO
+  Ingeniero ingeniero = Ingeniero("Ingenieria de Software");
+  print("\nIngeniero: " "${ingeniero.nombre} ${ingeniero.apellido} Edad: ${ingeniero.edad}");
+  print("Area: ${ingeniero.area}" );
+  ingeniero.color = "Rojo";
+  ingeniero.figura = "Triangulo";
+  print(ingeniero.dibujarFigura());
+  ingeniero.libro = "Cien Años de Soledad";
+  ingeniero.tema = "Drama";
+  print(ingeniero.leerLibro());
+  /////////////////// DOCTOR
+  Doctor doctor = Doctor("Pediatria");
+  print("\nDoctor: ${doctor.nombre} ${doctor.apellido} Edad: ${doctor.edad}");
+  print("Especialista: ${doctor.especialista}");
+  doctor.libro = "Historia de la Pediatria";
+  doctor.tema = "Medicina";
+  print(doctor.leerLibro());
+  doctor.deporte = "Futbol";
+  doctor.horas = "5 hr";
+  print(doctor.praticarDeporte());
+  /////////////////// ATLETA
+  Atleta atleta = Atleta("Alto");
+  print("\nAtleta: ${atleta.nombre} ${atleta.apellido} Edad: ${atleta.edad}");
+  print("Rendimiento: ${atleta.rendimiento}");
+  atleta.deporte = "Atletismo";
+  atleta.horas ="8 hr";
+  print(atleta.praticarDeporte());
+  atleta.golpea = true;
+  atleta.contrario = "Rick Ricon";
+  print(atleta.golpearContrario());
+  /////////////////// BOXEADOR
+  Boxeador boxeador = Boxeador("85 kg");
+  print("\nBoxeador: ${boxeador.nombre} ${boxeador.apellido} Edad: ${boxeador.edad}");
+  print("Rendimiento: ${boxeador.rendimiento} Peso: ${boxeador.peso}");
+  boxeador.deporte = "Boxeo";
+  boxeador.horas = "10 hr";
+  print(boxeador.praticarDeporte());
+  boxeador.golpea = false;
+  boxeador.contrario = "Mike Tyson";
+  print(boxeador.golpearContrario());
 }
 class Persona {
   String nombre;
@@ -17,15 +64,15 @@ class Artista extends Persona with Dibujar{
 }
 class Ingeniero extends Persona with Dibujar, Leer{
   String area;
-  Ingeniero(this.area): super.completo("Wilmer","Castro",23);
+  Ingeniero(this.area): super.completo("Arturo","Casas",23);
 }
 class Doctor extends Persona with Ejercitarse, Leer{
   String especialista;
-  Doctor(this.especialista): super.completo("Wilmer","Castro",23);
+  Doctor(this.especialista): super.completo("Pedro","Florez",23);
 }
 class Atleta extends Persona with Ejercitarse, Boxear{
   String rendimiento;
-  Atleta(this.rendimiento): super.completo("Wilmer","Castro",23);
+  Atleta(this.rendimiento): super.completo("Mario","Ruiz",23);
   Atleta.completo({required this.rendimiento, required super.nombre,required super.apellido, required super.edad});
 }
 class Boxeador extends Atleta {
@@ -54,9 +101,9 @@ mixin Ejercitarse {
   }
 }
 mixin Boxear {
-  Bool? golpea;
+  bool? golpea;
   String? contrario;
   golpearContrario(){
-    return "Golpe " + "${golpea}" + " a " + "${contrario}";
+    return "Golpe " + "${golpea == true ? "Directo": "Fallido" }" + " a " + "${contrario}";
   }
 }
